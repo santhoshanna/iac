dfaf23400f7bf4ae56dacfd04a61e69a92c72eae
Santosh UP <cups@onjci.com>
2018-05-17 14:17:02 -0500

    HTML with loader

--- cron.js
+++ cron.js
@@ -1,11 +1,8 @@
 var cron = require('node-cron')
-//var nodeimu  = require('nodeimu')
-//var IMU = new nodeimu.IMU()
+var nodeimu  = require('nodeimu')
+var IMU = new nodeimu.IMU()
 var tuc = require('temp-units-conv')
-var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString
-var Message = require('azure-iot-device').Message
 var connectionString = 'HostName=BH-IOT-HUB.azure-devices.net;DeviceId=BS-Raspberry-pi;SharedAccessKey=ZLQgsQwWu/+lSm5aNcL8STcOwDhGyXYrVBDyTe/0Kbg='
-var client = clientFromConnectionString(connectionString)
 var globalTPH = {
           "deviceId": "BS-Raspberry-pi",
           "temperarature": "",
@@ -13,43 +10,41 @@ var globalTPH = {
           "humidity": ""
    }
 
+var Mqtt = require('azure-iot-device-mqtt').Mqtt
+var DeviceClient = require('azure-iot-device').Client
+var Message = require('azure-iot-device').Message
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt)   
+
 var printResultFor = function (op) {
     return function printResult(err, res) {
-      if (err) console.log(op + ' error: ' + err.toString());
-      if (res) console.log(op + ' status: ' + res.constructor.name);
-    };
-  }
-
-var connectCallback = function (err) {
-    if (err) {
-        console.log('Could not connect to Azure IOT Hub: ' + err);
-    } else {
-            var data = JSON.stringify({ deviceId: 'BS-Raspberry-pi', temperature: globalTPH.temperature, humidity: globalTPH.humidity, pressure: globalTPH.pressure });
-            var message = new Message(data);
-            message.properties.add('temperatureAlert', (globalTPH.temperature > 30) ? 'true' : 'false');
-            console.log("Sending message: " + message.getData());
-            client.sendEvent(message, printResultFor('send'));
+          if (err) console.log(op + ' error: ' + err.toString());
+          if (res) console.log(op + ' status: ' + res.constructor.name);
     }
   }
+setInterval(function(){
+          var temperature = globalTPH.temperature
+          var humidity = globalTPH.humidity
+          var pressure = globalTPH.pressure
+          var deviceId = globalTPH.deviceId
+          var data = JSON.stringify({ deviceId:deviceId, temperature: temperature, humidity: humidity, pressure:pressure });
+          var message = new Message(data);
+          message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+          console.log('Sending message: ' + message.getData());
+          client.sendEvent(message, printResultFor('send'));
+}, 6000)
 
 var getTPH = function () {
 
   return new Promise((resolve, reject) =>{
           //console.log('In Get TPH Block');
           var data = IMU.getValueSync();
-          //var data = {"temperature": "90.000", "pressure": "90.000", "humidity": "90.000"}
           globalTPH.temperature=tuc.c2f(data.temperature.toFixed(0))
           globalTPH.pressure=data.pressure.toFixed(0)
           globalTPH.humidity=data.humidity.toFixed(0)
-
-          //globalTPH.temperature= "71";
-          //globalTPH.pressure="70";
-          //globalTPH.humidity="22";
           console.log("tph deviceId: " + globalTPH.deviceId);
           console.log("tph temperature: " + globalTPH.temperature);
           console.log("tph pressure: " + globalTPH.pressure);
           console.log("tph humidity: " + globalTPH.humidity);
-          //client.open(connectCallback);
           resolve(globalTPH)
   }).catch((error)=>{
           exit(`Completed with error ${JSON.stringify(error)}`)
@@ -61,20 +56,6 @@ var getTPH = function () {
 function exit(message) {
     console.log(message)
 }
-
-var task = cron.schedule('* * * * *', function() {
-          console.log('immediately started');
-          var data = IMU.getValueSync();
-          //var data = {temperature: "90.000", pressure: "90.000", humidity: "90.000"}
-          globalTPH.temperature=tuc.c2f(data.temperature.toFixed(0)) + "F";
-          globalTPH.pressure=data.pressure.toFixed(0) + "Pi";
-          globalTPH.humidity=data.humidity.toFixed(0) + "%";
-          console.log("tph deviceId: " + globalTPH.deviceId);
-          console.log("tph temperature: " + globalTPH.temperature);
-          console.log("tph pressure: " + globalTPH.pressure);
-          console.log("tph humidity: " + globalTPH.humidity);
-          client.open(connectCallback);
-}, false);
 
 var sensorToggle= function(state, callback) {
         //console.log("State Value: " + state)
--- local_cron.js
+++ local_cron.js
@@ -1,11 +1,10 @@
 var cron = require('node-cron')
 //var nodeimu  = require('nodeimu')
 //var IMU = new nodeimu.IMU()
+
+
 var tuc = require('temp-units-conv')
-var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString
-var Message = require('azure-iot-device').Message
 var connectionString = 'HostName=BH-IOT-HUB.azure-devices.net;DeviceId=BS-Raspberry-pi;SharedAccessKey=ZLQgsQwWu/+lSm5aNcL8STcOwDhGyXYrVBDyTe/0Kbg='
-var client = clientFromConnectionString(connectionString)
 var globalTPH = {
           "deviceId": "BS-Raspberry-pi",
           "temperarature": "",
@@ -13,35 +12,20 @@ var globalTPH = {
           "humidity": ""
    }
 
+var Mqtt = require('azure-iot-device-mqtt').Mqtt
+var DeviceClient = require('azure-iot-device').Client
+var Message = require('azure-iot-device').Message
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt)   
+
 var printResultFor = function (op) {
     return function printResult(err, res) {
-      if (err) console.log(op + ' error: ' + err.toString());
-      if (res) console.log(op + ' status: ' + res.constructor.name);
-    };
-  }
-
-var connectCallback = function (err) {
-    if (err) {
-        console.log('Could not connect to Azure IOT Hub: ' + err);
-    } else {
-            var data = JSON.stringify({ deviceId: 'BS-Raspberry-pi', temperature: globalTPH.temperature, humidity: globalTPH.humidity, pressure: globalTPH.pressure });
-            var message = new Message(data);
-            message.properties.add('temperatureAlert', (globalTPH.temperature > 30) ? 'true' : 'false');
-            console.log("Sending message: " + message.getData());
-            client.sendEvent(message, printResultFor('send'));
+          if (err) console.log(op + ' error: ' + err.toString());
+          if (res) console.log(op + ' status: ' + res.constructor.name);
     }
   }
 
 var getTPH = function () {
-
   return new Promise((resolve, reject) =>{
-          //console.log('In Get TPH Block');
-          //var data = IMU.getValueSync();
-          //var data = {"temperature": "90.000", "pressure": "90.000", "humidity": "90.000"}
-         /* globalTPH.temperature=tuc.c2f(data.temperature.toFixed(0))
-          globalTPH.pressure=data.pressure.toFixed(0)
-          globalTPH.humidity=data.humidity.toFixed(0)*/
-
           globalTPH.temperature= "75";
           globalTPH.pressure="70";
           globalTPH.humidity="22";
@@ -49,7 +33,6 @@ var getTPH = function () {
           console.log("tph temperature: " + globalTPH.temperature);
           console.log("tph pressure: " + globalTPH.pressure);
           console.log("tph humidity: " + globalTPH.humidity);
-          //client.open(connectCallback);
           resolve(globalTPH)
   }).catch((error)=>{
           exit(`Completed with error ${JSON.stringify(error)}`)
@@ -59,46 +42,43 @@ var getTPH = function () {
 
 // Exit function
 function exit(message) {
-    console.log(message)
+          console.log(message)
 }
 
-var task = cron.schedule('* * * * *', function() {
-          console.log('immediately started');
-          var data = IMU.getValueSync();
-          //var data = {temperature: "90.000", pressure: "90.000", humidity: "90.000"}
-          globalTPH.temperature=tuc.c2f(data.temperature.toFixed(0)) + "F";
-          globalTPH.pressure=data.pressure.toFixed(0) + "Pi";
-          globalTPH.humidity=data.humidity.toFixed(0) + "%";
-          console.log("tph deviceId: " + globalTPH.deviceId);
-          console.log("tph temperature: " + globalTPH.temperature);
-          console.log("tph pressure: " + globalTPH.pressure);
-          console.log("tph humidity: " + globalTPH.humidity);
-          client.open(connectCallback);
-}, false);
+setInterval(function(){
+          var temperature = globalTPH.temperature
+          var humidity = globalTPH.humidity
+          var pressure = globalTPH.pressure
+          var deviceId = globalTPH.deviceId
+          var data = JSON.stringify({ deviceId:deviceId, temperature: temperature, humidity: humidity, pressure:pressure });
+          var message = new Message(data);
+          message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+          console.log('Sending message: ' + message.getData());
+          client.sendEvent(message, printResultFor('send'));
+}, 6000)
 
 var sensorToggle= function(state, callback) {
         //console.log("State Value: " + state)
-        var response_json = {
-            "data": "",
-            "message": ""
-        } 
-        if(state === '1'){
-            task.start()
-            response_json.data = "Sensing Started"
-            response_json.message = "Sensing Started"
-            return callback(response_json);
-        }else{
-            task.stop()
-            response_json.data = "Sensing Stopped"
-            response_json.message = "Sensing Stopped"
-            return callback(response_json);
-        }
-        
+          var response_json = {
+              "data": "",
+              "message": ""
+          } 
+          if(state === '1'){
+              task.start()
+              response_json.data = "Sensing Started"
+              response_json.message = "Sensing Started"
+              return callback(response_json);
+          }else{
+              task.stop()
+              response_json.data = "Sensing Stopped"
+              response_json.message = "Sensing Stopped"
+              return callback(response_json);
+          }          
 }
 
 module.exports = {
-  sensorToggle: sensorToggle,
-  getTPH:getTPH
+              sensorToggle: sensorToggle,
+              getTPH:getTPH
 }
 
 
